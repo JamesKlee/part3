bf06c7b2ae90507820ec0455345c2c01196e3b01
James Klee <james.klee@outlook.com>
2015-11-18 14:04:33 +0000

    Server working for one map

--- src/pf_localisation/updateParticle.py
+++ src/pf_localisation/updateParticle.py
@@ -34,6 +34,8 @@ class UpdateParticleCloud():
 			if weight > maxWeight:
 				maxWeight = weight
 
+		print(maxWeight)
+
 		pf.weights = particleWeights		
 		pf.maxWeight = maxWeight
 		pf.totalWeight = totalWeight
@@ -111,7 +113,7 @@ class UpdateParticleCloud():
 		self.weight_particles(scan, pf)
 
 		#if the maximum weighted particle has a weight below 7 reinitialise the particles
-		if pf.maxWeight < 7:
+		if pf.maxWeight < 8:
 			pf.particlecloud = pf.reinitialise_cloud(pf.estimatedpose.pose.pose, 3.0, True)
 			self.weight_particles(scan, pf)
 
@@ -180,129 +182,3 @@ class UpdateParticleCloud():
 			resampledPose = (particle[0], particle[1])
 			resampledPoses.append(resampledPose)
 		return resampledPoses
-
-	#Updates particles according to KLD-AMCL
-	def update_kld_amcl(self, scan, pf):
-
-		self.weight_particles(scan, pf)
-
-		numParticles = len(pf.particlecloud.poses)
-
-		rospy.loginfo(maxWeight)
-
-		#if the maximum weighted particle has a weight below 10 reinitialise the particles
-		if maxWeight < 7:
-			pf.particlecloud = pf.reinitialise_cloud(pf.estimatedpose.pose.pose, 3.0, True)
-			self.weight_particles(scan, pf)
-			numParticles = len(pf.particlecloud.poses)
-
-
-		resampledPoses = []
-		index = 0
-		notAccepted = True
-
-		#Initialize KLD Sampling 	
-		ztable = [i/maxWeight for i in particleWeights]
-		support_samples=0
-		num_samples=0
-		quantile=0.5
-		kld_error = 0.1
-		bin_size = 0.1
-		min_samples=10
-		seed=-1
-		kld_samples = 0
-
-		if (min_samples < self.ABSOLUTE_MIN):
-			kld_samples=self.ABSOLUTE_MIN
-		else:
-			kld_samples=min_samples
-
-		bins = []
-
-		confidence=quantile-0.5; # ztable is from right side of mean
-		confidence=min(0.49998,max(0,confidence))
-
-		max_error = kld_error;
-		bin_size = bin_size; # list of lists
-
-		zvalue=4.1;
-		for i in range(len(ztable)):
-			if(ztable[i] >= confidence):
-				zvalue=i/100.0
-				break
-	
-		#Resample the poses
-		samples = []
-
-		while (num_samples < min_samples and num_samples < 250) :
-			#Get Sample
-			notAccepted = True
-			while (notAccepted):
-				index = random.randint(0,numParticles-1)
-				posX = pf.particlecloud.poses[index].position.x
-				posY = pf.particlecloud.poses[index].position.y
-
-				if (random.uniform(0,1) < particleWeights[index]/totalWeight):
-					notAccepted = False
-
-			curr_sample = pf.particlecloud.poses[index]
-			samples.append(curr_sample)
-			num_samples = num_samples+1
-			curr_bin = curr_sample
-
-			if len(bins)==0 or curr_bin not in bins:
-				bins.append(curr_bin);
-				support_samples = support_samples+1
-				if support_samples>=2:
-					k = support_samples-1
-					k=math.ceil(k/(2*max_error)*pow(1-2/(9.0*k)+math.sqrt(2/(9.0*k))*zvalue,3))
-					if k>kld_samples:
-					    kld_samples = k
-
-			min_samples = kld_samples
-			
-	
-		resampledPoses = samples
-		rospy.loginfo("Size Samples = %s"%len(samples))
-		cont = True
-		pArray = PoseArray()
-		temp = []
-		val = Pose()
-		count = 0
-			
-		# TEST this value, rounding scalar
-		scale = 0.66
-	
-		while cont:
-			temp = []
-			val = resampledPoses[0]
-			count = 0
-	
-			for i in range(0, len(resampledPoses)):
-				if (resampledPoses[i] == val):
-					count = count + 1
-				else:
-					temp.append(resampledPoses[i])
-	
-			resampledPoses = temp
-			if (len(resampledPoses) == 0):
-				cont = False
-						
-			# THIS NEEDS TESTS, look at scalar above
-			if (count > 4) and len(resampledPoses) >= 50: #TEST
-				#count = count - 2
-				count = int(count * scale)
-						
-			for i in range(0, count):
-				if i > 0:
-					newPose = Pose()
-					newPose.position.x = random.gauss(val.position.x, 0.3) #TEST THIS
-					newPose.position.y = random.gauss(val.position.y, 0.3) #TEST THIS
-					newPose.orientation = rotateQuaternion(val.orientation, random.vonmisesvariate(0, 4)) #TEST THIS
-					pArray.poses.append(newPose)
-						
-				else:
-					pArray.poses.append(val)
-
-		return pArray
-
