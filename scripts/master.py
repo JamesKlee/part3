9099f9fb80276f936d87b7fd9157db686eb980de
James Klee <james.klee@outlook.com>
2015-11-19 12:49:33 +0000

    Server now deals with two maps and assigns the correct register array in updateParticle

--- scripts/master.py
+++ scripts/master.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import ( PoseStamped, PoseWithCovarianceStamped,
                                 PoseArray, Quaternion )
 from pf_localisation.msg import WeightedParticles, Registration
 from pf_localisation.updateParticle import UpdateParticleCloud
+import thread
 
 import sys
 import time
@@ -20,6 +21,7 @@ class Node(object):
 		self.particlesAdded = []
 		self.totalWeight = 0
 		self.updater = UpdateParticleCloud()
+		self.lock = thread.allocate_lock()
 
 		self._cloud_publisher = rospy.Publisher("/updatedCloud", PoseArray)
 		self._weighted_particle_subscriber = rospy.Subscriber("/weightedParticles", WeightedParticles, self.addParticles, queue_size=1)
@@ -27,31 +29,35 @@ class Node(object):
 		rospy.loginfo("RUNNING")
 		
 	def register(self, reg):
+		self.lock.acquire()
 		nFound = True
 		pos = 0
 		for i in range (0, len(self.registered)):
-			if reg.frame_id == self.registered[i]:
+			if reg.frame_id == self.registered[i][0]:
 				nFound = False
 				pos = i
 
 		if nFound and reg.toAdd:
-			self.registered.append(reg)
+			self.registered.append((reg.frame_id, reg.freePoints, reg.resolution))
 			rospy.loginfo("\tREGISTERED: " + reg.frame_id)
 			self.updater.mapInfo = self.registered
 		elif not nFound and not reg.toAdd:
 			del self.registered[pos]
 			rospy.loginfo("\tDEREGISTERED: " + reg.frame_id)
+		self.lock.release()
 
 	def addParticles(self, wParticles):
+		self.lock.acquire()
 		name = wParticles.poseArray.header.frame_id
 		toAdd = False
 		posReg = None
 		for i in range(0, len(self.registered)):			
-			if self.registered[i].frame_id == name:
+			if self.registered[i][0] == name:
 				toAdd = True
 				posReg = i
 		if not toAdd:
 			print("NOT FOUND IN REGISTERED")
+			self.lock.release()
 			return
 		print("FOUND IN REGISTERED")
 		toAdd = True
@@ -59,6 +65,7 @@ class Node(object):
 			if self.particlesAdded[i] == name:
 				toAdd = False
 		if not toAdd:
+			self.lock.release()
 			return
 		rospy.loginfo("\tRECEIVED: " + name)
 
@@ -66,11 +73,13 @@ class Node(object):
 			newWT = (name, wParticles.poseArray.poses[i], wParticles.array[i])
 			self.particleWT.append(newWT)
 
-		self.totalWeight = wParticles.totalWeight
+		self.totalWeight = self.totalWeight + wParticles.totalWeight
 		self.particlesAdded.append(name)
 
 		if len(self.particlesAdded) == len(self.registered):
 			self.resample()
+		else:
+			self.lock.release()
 			
 		
 
@@ -79,7 +88,7 @@ class Node(object):
 		toSend = []
 		for i in range(0, len(self.registered)):
 			toAdd = []
-			toAdd.append(self.registered[i].frame_id)
+			toAdd.append(self.registered[i][0])
 			toSend.append(toAdd)
 
 		for i in range (0, len(particles)):
@@ -96,6 +105,13 @@ class Node(object):
 				list = toSend[i]
 				del list[0]
 			self.send(name,list)
+			time.sleep(0.075)
+
+		self.particleWT = []
+		self.particlesAdded = []
+		self.totalWeight = 0
+
+		self.lock.release()
 
 	def send(self, map_topic, particles):
 		rospy.loginfo("\tSENDING TO: " + map_topic)
@@ -105,10 +121,6 @@ class Node(object):
 		pArray.header.frame_id = map_topic
 		pArray.poses = particles
 		self._cloud_publisher.publish(pArray)
-		
-		self.particleWT = []
-		self.particlesAdded = []
-		self.totalWeight = 0
 
 rospy.init_node("master")
 Node()
