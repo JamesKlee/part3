54b9455143acf8c44f6c602119eec066b9d21634
Amar Saggu <amar.singh.saggu@gmail.com>
2015-11-28 13:48:55 +0000

    Improve decider
    The decider now waits for all floors to be publishing before
    starting

--- scripts/decider.py
+++ scripts/decider.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import math
+import sys
 import rospy
 from geometry_msgs.msg import Pose
 from pf_localisation.msg import Cluster
@@ -11,23 +12,30 @@ posterior = {}
 
 def cluster_callback(c):
 	floor = c.floorName
-	
 	likelihood[floor] = c.pointsInCluster / float(c.totalPoints)
-	
-	if not posterior.has_key(floor):
-		posterior[floor] = 0.5
 
-def main():
+def decider(floorCount):
 	rospy.init_node("cluster_decider")
 	rospy.Subscriber("cluster", Cluster, cluster_callback)
 	
 	rate = rospy.Rate(2) # 2hz
 	
+	rospy.loginfo("waiting for " + str(floorCount) + " floor(s)")
+	
+	while len(likelihood) != floorCount and not rospy.is_shutdown():
+		rospy.sleep(0.1)
+	
+	rospy.loginfo("received messages from all floors")
+	
+	# initally an equal probability for all floors
+	for (floor, _) in likelihood.items():
+		posterior[floor] = 1.0 / floorCount
+	
 	while not rospy.is_shutdown():
-		total = 0
+		total = 0.0
 		
+		# multiply by the previous probability
 		for (floor, val) in posterior.items():
-			# multiply by the previous probability
 			posterior[floor] *= likelihood[floor]
 			total += posterior[floor]
 		
@@ -35,14 +43,23 @@ def main():
 		for (floor, val) in posterior.items():
 			posterior[floor] /= total
 		
-		# get the floor with the largest probability
-		if len(posterior):
-			print("")
-			print(posterior)
-			guess = max(posterior, key=(lambda k: posterior[k]))
-			rospy.loginfo("I'm probably on the " + guess)
+		# find the floor with the largest probability
+		print("")
+		print(posterior)
+		guess = max(posterior, key=(lambda k: posterior[k]))
+		rospy.loginfo("I'm probably on the " + guess)
 		
 		rate.sleep()
 
+def main(argv):
+	if len(argv) != 2:
+		print("invalid number of arguments")
+		print("usage: " + argv[0] + " <floor count>")
+		sys.exit(0)
+	
+	floorCount = int(argv[1])
+	
+	decider(floorCount)
+
 if __name__ == "__main__":
-	main()
+	main(sys.argv)
